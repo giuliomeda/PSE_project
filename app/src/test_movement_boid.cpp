30a595cd3ec9335ce5f26d4e22a09c9c59391b61
Giulio Meda <giulio.meda@studenti.unipd.it>
2023-12-21 17:04:21 +0100

    modifica CMake file e file di test per il movimento del singolo boid

--- app/src/test_movement_boid.cpp
+++ app/src/test_movement_boid.cpp
@@ -4,16 +4,17 @@
 using std::thread;
 
 
-boid my_robot{};
+boid my_robot{0,0,5,5};
+std::list<boid> neighbors;
 
 
 void handle_robot(){
     int number_update{0};
 
     while (number_update < 1000){
-        my_robot.update_positon();
-        /*std::cout << "Pos: " << my_robot.get_pos_x_() << ", " << my_robot.get_pos_y_() << "\n";
-        std::cout << "Vel: " << my_robot.get_vel_x_() << ", " << my_robot.get_vel_y_() << "\n";*/
+        my_robot.update_positon(neighbors);
+        std::cout << "Pos: " << my_robot.get_pos_x_() << " " << my_robot.get_pos_y_() << "\n";
+        //std::cout << "Vel: " << my_robot.get_vel_x_() << ", " << my_robot.get_vel_y_() << "\n";
         number_update ++;
     }
     return;
@@ -25,6 +26,7 @@ int main(){
     std::cout << "Dimensioni dello schermo: " << boid::get_screen_width() << ", " << boid::get_screen_height() << "\n";
     std::cout << "Posiziione iniziale robot: " << my_robot.get_pos_x_() << ", " << my_robot.get_pos_y_() << "\n";
     std::cout << "Velocità iniziale robot: " << my_robot.get_vel_x_() << ", " << my_robot.get_vel_y_() << "\n";
+    std::cout << "neighbors:" << neighbors.size() << " \n";
 
     thread robot_manager{handle_robot};
 
